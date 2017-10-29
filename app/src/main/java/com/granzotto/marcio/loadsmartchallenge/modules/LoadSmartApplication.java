a1a08e63448e21b9e25655473d03ea185eaed87d
Marcio <marcio@ckl.io>
2017-10-29 14:52:36 -0200

    Adds Realm to the project

---
+++ app/src/main/java/com/granzotto/marcio/loadsmartchallenge/modules/LoadSmartApplication.java
@@ -0,0 +1,24 @@
+package com.granzotto.marcio.loadsmartchallenge.modules;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+
+public class LoadSmartApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		setupRealm();
+	}
+
+	private void setupRealm() {
+		Realm.init(this);
+		RealmConfiguration config = new RealmConfiguration.Builder()
+				.schemaVersion(0)
+				.build();
+		Realm.setDefaultConfiguration(config);
+	}
+}
