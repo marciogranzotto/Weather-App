2ff7a61b8423c00b93c4b492e13a430b360901fe
Marcio <marcio@ckl.io>
2017-11-05 16:35:15 -0200

    Implements happy test on saving city

--- app/src/test/java/com/granzotto/marcio/loadsmartchallenge/AddCityPresenterTest.java
+++ app/src/test/java/com/granzotto/marcio/loadsmartchallenge/AddCityPresenterTest.java
@@ -5,12 +5,16 @@ import android.content.Context;
 import com.granzotto.marcio.loadsmartchallenge.modules.add_city.AddCityContracts;
 import com.granzotto.marcio.loadsmartchallenge.modules.add_city.AddCityPresenter;
 import com.granzotto.marcio.loadsmartchallenge.utils.datamanagers.CityDBDataManager;
+import com.granzotto.marcio.loadsmartchallenge.utils.datamanagers.FlickrApiDataManager;
 import com.granzotto.marcio.loadsmartchallenge.utils.datamanagers.WeatherApiDataManager;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -22,6 +26,7 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
+import io.reactivex.Observable;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import io.realm.RealmQuery;
@@ -30,7 +35,11 @@ import io.realm.internal.RealmCore;
 import io.realm.log.RealmLog;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.doNothing;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
@@ -53,6 +62,8 @@ public class AddCityPresenterTest {
 	private CityDBDataManager dbDataManager;
 	@Mock
 	private WeatherApiDataManager weatherDataManager;
+	@Mock
+	private FlickrApiDataManager flickrApiDataManager;
 
 	private AddCityPresenter presenter;
 
@@ -75,7 +86,34 @@ public class AddCityPresenterTest {
 		whenNew(RealmConfiguration.class).withAnyArguments().thenReturn(mockRealmConfig);
 		when(Realm.getDefaultInstance()).thenReturn(mockRealm);
 
-		presenter = new AddCityPresenter(view, weatherDataManager, dbDataManager);
+		presenter = new AddCityPresenter(view, weatherDataManager, dbDataManager, flickrApiDataManager);
 	}
 
+	@Test
+	public void saveButtonClickedShouldSaveAndFinishScreen() {
+		String cityName = "New York";
+		String stateName = "NY";
+		String weatherID = "42";
+		String imgUrl = "www.google.com";
+
+		when(weatherDataManager.fetchCityId(cityName))
+				.thenReturn(Observable.just(weatherID));
+
+		when(flickrApiDataManager.fetchImageUrl(cityName + ", " + stateName))
+				.thenReturn(Observable.just(imgUrl));
+
+		doAnswer(invocation -> {
+			CityDBDataManager.SaveListener listener = invocation.getArgumentAt(1, CityDBDataManager.SaveListener.class);
+			listener.onSuccess();
+			return invocation;
+		}).when(dbDataManager).saveCity(any(), any());
+
+		presenter.onSaveButtonClicked(cityName, stateName);
+
+		InOrder inOrder = Mockito.inOrder(view);
+		inOrder.verify(view, times(1)).showLoadingDialog();
+		inOrder.verify(view, times(1)).hideLoadingDialog();
+		inOrder.verify(view, times(1)).closeScreen();
+		verify(view, never()).showErrorDialog(any());
+	}
 }
